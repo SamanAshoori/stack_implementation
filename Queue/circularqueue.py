adbde48ac387fa12a572208b493359da30648cb7
Saman Ashoori <saman@samanashoori.com>
2025-11-29 23:57:01 +0000

    fix: correct initialization method name in Queue class

--- Queue/circularqueue.py
+++ Queue/circularqueue.py
@@ -3,5 +3,13 @@ class Queue:
         self.size = size
         self.front = -1
         self.tail = -1
+        self.isEmpty = True
         #To simulate C++ memory management
         self.data = ['None'] * size
+    
+    def enqueue(self,value:int) -> bool:
+        #input a int but return a boolean value
+        if(self.isEmpty == True):
+            self.front = 0
+            self.tail = 0
+            self.data

45ff30955f960933a2ac36554e0d1537ff6db1ea
Saman Ashoori <saman@samanashoori.com>
2025-11-30 00:08:09 +0000

    fix: simplify condition checks in enqueue method and ensure proper handling of isFull state

--- Queue/circularqueue.py
+++ Queue/circularqueue.py
@@ -4,17 +4,24 @@ class CircQueue:
         self.front = -1
         self.tail = -1
         self.isEmpty = True
+        self.isFull = False
         #To simulate C++ memory management
         self.data = ['None'] * size
     
     def enqueue(self,input:int) -> bool:
         #input a int but return a boolean value
-        if(self.isEmpty == True):
+        if(self.isEmpty):
             self.front = self.front + 1
             self.tail = self.tail + 1
             self.data[self.tail] = input
             self.isEmpty = False
             return True
+        elif(self.isFull):
+            return  False
+        else:
+            self.tail = self.tail + 1
+            self.data[self.tail] = input
+
 
 q1 = CircQueue(4)
 print(q1.enqueue(4))
